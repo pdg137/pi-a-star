65e2ebf7040f3fd0ffa1fc575cc331254a63f51a
Paul Grayson <pdg@alum.mit.edu>
2014-07-30 23:28:56 +0000

    added heartbeat led

--- pi/a_star.rb
+++ pi/a_star.rb
@@ -12,4 +12,11 @@ class AStar
   def get_report
     return @i2c.read(20,10).unpack("ccLL")
   end
+
+  def set_leds(red,yellow,green)
+    led = (red ? 1 : 0) +
+      (yellow ? 2 : 0 ) +
+      (green ? 4 : 0)
+    @i2c.write(20, [5,1,led].pack("CCC"))
+  end
 end
--- pi/robot.rb
+++ pi/robot.rb
@@ -2,6 +2,15 @@
 
 system("echo #{Process.pid} > /var/log/web.pid")
 
+require_relative 'a_star'
+
+a_star = AStar.new
+
 while true
-  sleep 1
+  a_star.set_leds(true,false,false)
+  sleep 0.2
+  a_star.set_leds(false,false,true)
+  sleep 0.2
+  a_star.set_leds(false,false,false)
+  sleep 0.6
 end
