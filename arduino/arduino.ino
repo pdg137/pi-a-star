e1db95605c082bdc0bb6cb00e8ceca758022b35e
Paul Grayson <paul@pololu.com>
2014-08-02 16:07:38 -0700

    added line detection

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -13,12 +13,13 @@ struct
 
 struct
 {
-  int8_t count1;
-  int8_t count2;
+  int32_t distance;
   uint32_t error1;
   uint32_t error2;
   uint8_t button_state;
   uint8_t sensors[5];
+  int16_t pos;
+  uint8_t detected_left, detected_right;
 } report;
 
 
@@ -40,10 +41,19 @@ void setup()
 
 #define limit(x,a,b) (max(min((x),(b)),(a)))
 
+uint8_t follow_line;
+int16_t last_pos;
+uint8_t off_line;
+int32_t off_line_distance;
+uint8_t detect_left_count;
+uint8_t detect_right_count;
+int32_t detected_left_distance;
+int32_t detected_right_distance;
+
 void loop()
 {
-  report.count1 = Encoders::count1;
-  report.count2 = Encoders::count2;
+  Encoders::update();
+  report.distance = Encoders::distance;
   report.error1 = Encoders::error1;
   report.error2 = Encoders::error2;
   
@@ -53,11 +63,67 @@ void loop()
     report.sensors[i] = (limit(analogRead(i+1),calibration_min[i],calibration_max[i])-calibration_min[i])*(uint32_t)255/(calibration_max[i]-calibration_min[i]);
   }
   
+  if(max(max(report.sensors[1],report.sensors[2]),report.sensors[3]) < 20)
+  {
+    // off line
+    report.pos = 0;
+    if(!off_line)
+    {
+      off_line = 1;
+      off_line_distance = Encoders::distance;
+    }
+  }
+  else
+  {
+    off_line = 0;
+    report.pos = (-(int32_t)report.sensors[1]+report.sensors[3])*1000/(report.sensors[1]+report.sensors[2]+report.sensors[3]);
+  }
+  
+  if(report.sensors[0] > 128)
+    detect_left_count += 1;
+  else
+    detect_left_count = 0;
+  if(detect_left_count > 10)
+    detected_left_distance = Encoders::distance;
+  if(report.sensors[4] > 128)
+    detect_right_count += 1;
+  else
+    detect_right_count = 0;
+  if(detect_right_count > 10)
+    detected_right_distance = Encoders::distance;
+  
+  if(off_line && Encoders::distance - off_line_distance > 600)
+  {
+    follow_line = 0;
+    
+    // check for exits
+    report.detected_left = (Encoders::distance - detected_left_distance < 1000);
+    report.detected_right = (Encoders::distance - detected_right_distance < 1000);
+  }
+  
   report.button_state = (digitalRead(18) ? 0 : 1) +
     (digitalRead(15) ? 0 : 2) +
     (digitalRead(14) ? 0 : 4);
   
-  Motors::set(data.motors[0],data.motors[1]);
+  if(report.button_state & 2)
+    follow_line = 1;
+  
+  if(follow_line)
+  {
+    int32_t diff = last_pos - report.pos;
+    
+    int32_t pid = report.pos/4 - diff;
+    Motors::set(limit(100+pid,0,100), limit(100-pid,0,100));
+    
+    last_pos = report.pos;
+    report.detected_left = report.detected_right = 0;
+  }
+  else
+  {
+    off_line = 0;
+    detected_left_distance = detected_right_distance = 0;
+    Motors::set(0,0);
+  }
 
   if(data.led_state & 1) RXLED1;
   else RXLED0;
--- arduino/encoders.cpp
+++ arduino/encoders.cpp
@@ -5,38 +5,40 @@ int8_t Encoders::count1 = 0;
 int8_t Encoders::count2 = 0;
 uint32_t Encoders::error1 = 0;
 uint32_t Encoders::error2 = 0;
+int32_t Encoders::distance = 0;
 
 uint8_t last11, last12, last21, last22;
 
-// Encoder 1 uses INT2 and INT3
+// Encoder two uses INT2 and INT3 (digital pins 0 and 1)
 ISR(INT3_vect,ISR_ALIASOF(INT2_vect));
 ISR(INT2_vect)
 {
-  uint8_t new11 = ((PIND & 0x08) != 0);
-  uint8_t new12 = ((PIND & 0x04) != 0);
+  uint8_t new21 = ((PIND & 0x08) != 0);
+  uint8_t new22 = ((PIND & 0x04) != 0);
   
-  Encoders::count1 += (last11 ^ new12) - (int)(new11 ^ last12);
+  Encoders::count2 += (last21 ^ new22) - (int)(new21 ^ last22);
+  
+  if((last21 ^ new21) & (last22 ^ new22))
+    Encoders::error1 ++;
+    
+  last21 = new21;
+  last22 = new22;
+}
+
+// Encoder one uses PCINT4 and PCINT7 (which trigger the PCINT0 interrupt)
+// (digital pins 8 and 11)
+ISR(PCINT0_vect)
+{
+  uint8_t new11 = ((PINB & 0x10) != 0);
+  uint8_t new12 = ((PINB & 0x80) != 0);
+  
+  Encoders::count1 += (last11 ^ new12) - (int)(new11 ^ last12);  
   
   if((last11 ^ new11) & (last12 ^ new12))
     Encoders::error1 ++;
-    
+  
   last11 = new11;
   last12 = new12;
-}
-
-// Encoder two uses PCINT4 and PCINT7 (which trigger the PCINT0 interrupt)  
-ISR(PCINT0_vect)
-{
-  uint8_t new21 = ((PINB & 0x10) != 0);
-  uint8_t new22 = ((PINB & 0x80) != 0);
-  
-  Encoders::count2 += (last21 ^ new22) - (int)(new21 ^ last22);  
-  
-  if((last21 ^ new21) & (last22 ^ new22))
-    Encoders::error2 ++;
-  
-  last21 = new21;
-  last22 = new22;
 }
 
 void Encoders::setup()
@@ -50,4 +52,20 @@ void Encoders::setup()
   EIMSK = 0x0c; // enable INT2 and INT3
   EIFR = 0; // clear interrupt flags
   sei();
+}
+
+void Encoders::update()
+{
+  cli();
+  if(count1 != 0 || count2 != 0)
+  {
+    distance += count1+count2;
+    count1 = count2 = 0;
+  }
+  sei();
+}
+
+void Encoders::reset()
+{
+  distance = 0;
 }
