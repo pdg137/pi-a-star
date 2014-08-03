e6e7c240af96655777740b4d5d84e028647af57e
Paul Grayson <pdg@alum.mit.edu>
2014-08-03 00:35:58 +0000

    pi code to issue commands

--- pi/a_star.rb
+++ pi/a_star.rb
@@ -5,8 +5,24 @@ class AStar
     @i2c = I2C.create('/dev/i2c-1')
   end
 
-  def set_motors(m1, m2)
-    @i2c.write(20, [0,1,m1,m2].pack("CCss"))
+  def send_command(command)
+    @i2c.write(20, [0,1,command].pack("CCC"))
+  end
+
+  def turn_left
+    send_command(1)
+  end
+
+  def turn_right
+    send_command(2)
+  end
+
+  def turn_back
+    send_command(3)
+  end
+
+  def follow
+    send_command(4)
   end
 
   def get_report
@@ -17,6 +33,6 @@ class AStar
     led = (red ? 1 : 0) +
       (yellow ? 2 : 0 ) +
       (green ? 4 : 0)
-    @i2c.write(20, [5,1,led].pack("CCC"))
+    @i2c.write(20, [2,1,led].pack("CCC"))
   end
 end
--- pi/run.rb
+++ pi/run.rb
@@ -1,13 +1,4 @@
 require_relative 'a_star'
 
 a_star = AStar.new
-a_star.set_motors(400,400);
-
-array = (0..400).to_a + (0..400).to_a.reverse + (-400..0).to_a.reverse + (-400..0).to_a
-
-array.each do |speed|
-  a_star.set_motors(speed, speed)
-end
-
-a_star.set_motors(0,0);
-
+a_star.turn_right
