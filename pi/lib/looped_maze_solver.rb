e4ac937c375cada6e331de9ed094c94084ee3b12
Paul Grayson <paul@pololu.com>
2014-08-06 17:07:29 -0700

    add deductions to allow us to skip some unexplored nodes

--- pi/lib/looped_maze_solver.rb
+++ pi/lib/looped_maze_solver.rb
@@ -2,12 +2,13 @@ require_relative 'maze'
 require_relative 'point'
 require_relative 'vector'
 require_relative 'gridded_maze'
+require_relative 'segment_voter'
 require 'set'
 
 class LoopedMazeSolver
   # Our internal concept of the maze.
   # Updated as we explore it.
-  attr_reader :maze, :pos, :vec, :explored_nodes, :a_star
+  attr_reader :maze, :pos, :vec, :explored_nodes, :a_star, :voter, :deduced_nodes
 
   def set_initial_position
     @pos = Point(0,0)
@@ -18,7 +19,9 @@ class LoopedMazeSolver
     @a_star = a_star
     set_initial_position
     @explored_nodes = Set.new
+    @deduced_nodes = Set.new
     @maze = GriddedMaze.new
+    @voter = SegmentVoter.new
 
     # assume we start facing a segment
     maze.connect(pos, pos+vec)
@@ -28,8 +31,10 @@ class LoopedMazeSolver
     maze.nodes - explored_nodes
   end
 
-  def unexplored_other_nodes
-    unexplored_nodes.reject { |node| pos == node }
+  def useful_nodes_to_explore
+    unexplored_nodes.reject { |node|
+      pos == node || deduced_nodes.include?(node)
+    }
   end
 
   def estimate_grid_units(distance)
@@ -41,31 +46,92 @@ class LoopedMazeSolver
     puts "connect #{a}-#{b}"
   end
 
+  def disconnect(a,b)
+    maze.disconnect(a,b)
+    puts "disconnect #{a}-#{b}"
+  end
+
+  # true if all possible segments are known to either exist or not,
+  # according to the voter
+  def check_deductions(node)
+    return if @explored_nodes.include? node
+
+    all_neighbors_known = node.cartesian_neighbors.map do |neighbor|
+      voter.known?(node, neighbor)
+    end.inject(:&)
+
+    if all_neighbors_known
+      @deduced_nodes << node
+    end
+  end
+
+  def observe_segment(a,b)
+    voter.vote_connected(a,b)
+
+    if voter.connected?(a,b)
+      connect(a,b)
+    end
+
+    check_deductions(a)
+    check_deductions(b)
+  end
+
+  def observe_no_segment(a,b)
+    puts "no segment #{a}-#{b}"
+    voter.vote_not_connected(a,b)
+    puts "-> #{voter.known?(a,b)}"
+
+    if voter.not_connected?(a,b) && maze.nodes.include?(a) && maze.nodes.include?(b)
+      disconnect(a,b)
+    end
+
+    check_deductions(a)
+    check_deductions(b)
+  end
+
   def record_path(context)
     units = estimate_grid_units context[:distance]
     puts "distance #{context[:distance]} -> #{units} units"
     # TODO: handle zero!
 
+    original_pos = @pos
     units.times.each do
       old_pos = pos
       @pos += vec
 
       puts "at #{pos} #{vec}"
 
-      connect(old_pos, pos)
+      observe_segment(old_pos, pos)
 
       puts maze.to_s(pos)
 
       explored_nodes << pos
     end
+
+    pos1 = original_pos
+    left = vec.turn(:left)
+    right = vec.turn(:left)
+    (units-1).times.each do
+      pos2 = pos1 + vec
+
+      left_node = pos2 + left
+      right_node = pos2 + right
+      observe_no_segment(pos2, left_node)
+      observe_no_segment(pos2, right_node)
+
+      pos1 = pos2
+    end
   end
 
   def record_intersection(context)
     puts "exits: #{context[:exits]}"
-    context[:exits].each do |dir|
-      next if dir == :back # this is already covered
+    [:left,:right,:straight].each do |dir|
       exit_vec = vec.turn(dir)
-      connect(pos, pos+exit_vec)
+      if(context[:exits].include? dir)
+        observe_segment(pos, pos+exit_vec)
+      else
+        observe_no_segment(pos, pos+exit_vec)
+      end
     end
   end
 
@@ -114,7 +180,7 @@ class LoopedMazeSolver
 
       distances_from_pos = maze.solve(pos)
 
-      closest_unexplored_node = unexplored_other_nodes.min_by { |node|
+      closest_unexplored_node = useful_nodes_to_explore.min_by { |node|
         distances_from_pos[node]
       }
 
--- pi/spec/looped_maze_solver_spec.rb
+++ pi/spec/looped_maze_solver_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe LoopedMazeSolver do
+  subject { LoopedMazeSolver.new(fake_a_star) }
+
   context "maze 1" do
     let(:fake_a_star) do
       FakeAStar.new <<END
@@ -13,8 +15,6 @@ describe LoopedMazeSolver do
 #-# #-#-#-#-#
 END
     end
-
-    subject { LoopedMazeSolver.new(fake_a_star) }
 
     it "solves the maze" do
       fake_a_star.goto Point(0,0), Vector(1,0)
@@ -35,6 +35,29 @@ END
 
       subject.explore_to_end
       expect(fake_a_star.pos).to eq fake_a_star.maze.end
+    end
+  end
+
+  context "easily optimizable maze" do
+    let(:fake_a_star) do
+      FakeAStar.new <<END
+#-#-#
+|   |
+#-# #
+|   |
+X #-#
+END
+    end
+
+    before do
+      fake_a_star.goto Point(1,0), Vector(1,0)
+    end
+
+    it "solves without exploring the central node (0,1)" do
+      subject.explore_to_end
+      expect(fake_a_star.pos).to eq fake_a_star.maze.end
+      subject.explore_entire_maze
+      expect(subject.explored_nodes).to_not include Point(0,1)
     end
   end
 end
