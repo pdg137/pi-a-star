17531a148eae1e4ca12e3da6e4997e0b06d77efc
Paul Grayson <pdg@alum.mit.edu>
2015-05-11 00:09:46 -0700

    simplified to remove maze code, starting to make new interface

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -11,40 +11,15 @@
 #define COMMAND_TURN_BACK 3
 #define COMMAND_FOLLOW 4
 
-struct
-{
-  uint8_t command_count;
-  uint8_t command;
-  uint32_t follow_min_distance;
-  uint8_t led_state_modified;
-  uint8_t led_state;
-} data;
-
-
-struct
-{
-  uint8_t command_count;
-  int32_t distance;
-  uint32_t error1;
-  uint32_t error2;
-  uint8_t button_state;
-  uint8_t sensors[5];
-  int16_t pos;
-  uint8_t follow_state;
-  uint8_t detected_left, detected_straight, detected_right, detected_end;
-} report;
+uint8_t input[15];
+uint8_t output[15];
 
 void setup()
 {
   pinMode(13, OUTPUT);
-  pinMode(18, INPUT_PULLUP);
-  pinMode(15, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
   
-  I2cInterface::setup(20, (uint8_t *)(&data), sizeof(data),
-    (uint8_t *)(&report), sizeof(report));
-  Motors::setup(4,5);
-  Encoders::setup();
+  I2cInterface::setup(20, input, sizeof(input),
+    output, sizeof(output));
 }
 
 void check_command()
@@ -52,61 +27,19 @@ void check_command()
   uint8_t new_command=COMMAND_NONE;
   
   cli();
-  if(data.command_count != report.command_count)
-  {
-    new_command = data.command;
-  }
+  // do some sensitive stuff here
   sei();
-  
-  report.command_count = data.command_count;
-  
-  switch(new_command)
-  {
-  case COMMAND_TURN_LEFT:
-    Follow::doTurn(-90);
-    break;
-  case COMMAND_TURN_RIGHT:
-    Follow::doTurn(90);
-    break;
-  case COMMAND_TURN_BACK:
-    Follow::doTurn(-180);
-    break;
-  case COMMAND_FOLLOW:
-    Follow::doFollow(data.follow_min_distance);
-    break;
-  }
 }
 
 void loop()
 {
-  Encoders::update();
-  Buttons::update();    
-  Follow::update();
-  
-  report.button_state = (Buttons::button0) +
-    (Buttons::button1 * 2) +
-    (Buttons::button2 * 4);
-  report.distance = Encoders::distance;
-  report.error1 = Encoders::error1;
-  report.error2 = Encoders::error2;
- 
-   uint8_t i;
-   for(i=0;i<5;i++)
-     report.sensors[i] = Follow::sensors[i];
-   report.pos = Follow::pos;
-   report.follow_state = Follow::state;
-   report.detected_left = Follow::detected_left;
-   report.detected_straight = Follow::detected_straight;
-   report.detected_right = Follow::detected_right;
-   report.detected_end = Follow::detected_end;
- 
-  if(data.led_state & 1) RXLED1;
+  if(input[0] & 1) RXLED1;
   else RXLED0;
   
-  if(data.led_state & 2) digitalWrite(13, HIGH);
-  else digitalWrite(13, LOW);
+//  if(input[0] & 2) digitalWrite(13, HIGH);
+//  else digitalWrite(13, LOW);
   
-  if(data.led_state & 4) TXLED1;
+  if(input[0] & 4) TXLED1;
   else TXLED0;
   
   check_command();
--- arduino/buttons.h
+++
@@ -1,8 +0,0 @@
-class Buttons
-{
-  public:
-  static void update();
-  static uint8_t button0;
-  static uint8_t button1;
-  static uint8_t button2;
-};
