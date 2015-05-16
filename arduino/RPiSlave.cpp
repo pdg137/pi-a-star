68d52c4bbb4a79fcca7c744a4da6130c62feb982
Paul Grayson <pdg@alum.mit.edu>
2015-05-16 11:15:04 -0700

    basic i2c event loop

--- arduino/RPiSlave.cpp
+++ arduino/RPiSlave.cpp
@@ -1,10 +1,25 @@
 #include "Arduino.h"
 #include "RpiSlave.h"
+#include <util/twi.h>
 
 void RPiSlave::init(unsigned char address)
 {
   TWAR = address << 1;
   ack();
+}
+
+void leds(char y, char g, char r)
+{
+  digitalWrite(13, y);
+  if(g)
+    TXLED1;
+  else
+    TXLED0;
+    
+  if(r)
+    RXLED1;
+  else
+    RXLED0;
 }
 
 ISR(TWI_vect)
@@ -36,6 +51,13 @@ uint8_t RPiSlave::handle_event(unsigned char event, unsigned char data)
 {
   switch(event)
   {
+    case TW_SR_DATA_ACK:
+      leds((TWDR & 1) != 0, (TWDR & 2) != 0, (TWDR & 4) != 0);
+      return 0;
+    case TW_SR_DATA_NACK:
+      return 1;
+    case TW_ST_DATA_ACK:
+      TWDR = 0; // put data here
   }
   
   return 0; // default - ACK
