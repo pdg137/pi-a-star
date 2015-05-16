2ec8f87191a5925239b97933b8d75374a86a5fa8
Paul Grayson <pdg@alum.mit.edu>
2015-05-16 09:59:59 -0700

    removed a lot of unnecessary code

--- arduino/rpi_twi.c
+++ arduino/rpi_twi.c
@@ -19,11 +19,6 @@
   Modified 2012 by Todd Krein (todd@krein.org) to implement repeated starts
 */
 
-#include <math.h>
-#include <stdlib.h>
-#include <inttypes.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
 #include <compat/twi.h>
 #include "Arduino.h" // for digitalWrite
 
@@ -41,15 +36,6 @@
 static void (*twi_onSlaveTransmit)(void);
 static void (*twi_onSlaveReceive)(uint8_t*, int);
 
-static uint8_t twi_txBuffer[TWI_BUFFER_LENGTH];
-static volatile uint8_t twi_txBufferIndex;
-static volatile uint8_t twi_txBufferLength;
-
-static uint8_t twi_rxBuffer[TWI_BUFFER_LENGTH];
-static volatile uint8_t twi_rxBufferIndex;
-
-static volatile uint8_t twi_error;
-
 /* 
  * Function twi_init
  * Desc     readys twi pins and sets twi bitrate
@@ -65,13 +51,7 @@ void twi_init(void)
   // initialize twi prescaler and bit rate
   cbi(TWSR, TWPS0);
   cbi(TWSR, TWPS1);
-  TWBR = ((F_CPU / TWI_FREQ) - 16) / 2;
-
-  /* twi bit rate formula from atmega128 manual pg 204
-  SCL Frequency = CPU Clock Frequency / (16 + (2 * TWBR))
-  note: TWBR should be 10 or higher for master mode
-  It is 72 for a 16mhz Wiring board with 100kHz TWI */
-
+  
   // enable twi module, acks, and twi interrupt
   TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA);
 }
@@ -94,45 +74,16 @@ void twi_setAddress(uint8_t address)
  * Input    ack: byte indicating to ack or to nack
  * Output   none
  */
-void twi_reply(uint8_t ack)
+void ack()
 {
-  // transmit master read ready signal, with or without ack
-  if(ack){
-    TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
-  }else{
-	  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
-  }
+  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT) | _BV(TWEA);
 }
 
-/* 
- * Function twi_stop
- * Desc     relinquishes bus master status
- * Input    none
- * Output   none
- */
-void twi_stop(void)
+void nack()
 {
-  // send stop condition
-  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWSTO);
-
-  // wait for stop condition to be exectued on bus
-  // TWINT is not set after a stop condition!
-  while(TWCR & _BV(TWSTO)){
-    continue;
-  }
+  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWINT);
 }
 
-/* 
- * Function twi_releaseBus
- * Desc     releases bus control
- * Input    none
- * Output   none
- */
-void twi_releaseBus(void)
-{
-  // release bus
-  TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT);
-}
 
 void leds(uint8_t y, uint8_t g, uint8_t r)
 {
@@ -157,7 +108,7 @@ ISR(TWI_vect)
     case TW_SR_ARB_LOST_SLA_ACK:   // lost arbitration, returned ack
     case TW_SR_ARB_LOST_GCALL_ACK: // lost arbitration, returned ack
       // indicate that rx buffer can be overwritten and ack
-      twi_reply(1);
+      ack();
       break;
     case TW_SR_DATA_ACK:       // data received, returned ack
       delayMicroseconds(10);
@@ -165,38 +116,24 @@ ISR(TWI_vect)
       leds((TWDR & 1) != 0, (TWDR & 2) != 0, (TWDR & 4) != 0);
       // if there is still room in the rx buffer
       // put byte in buffer and ack
-      //twi_rxBuffer[twi_rxBufferIndex++] = TWDR;
-      twi_reply(1);
-      // otherwise nack
-      //twi_reply(0);
+      ack(); // or nack() if no room
       break;
-    case TW_SR_STOP: // stop or repeated start condition received
-      // this is where we would do clock stretching, but RPi does not support it
-      //twi_stop();
-      
+    case TW_SR_STOP: // stop or repeated start condition received      
       // ack future responses and leave slave receiver state
-      twi_releaseBus();
+      ack();
       
-      // callback to user defined callback
-      //twi_onSlaveReceive(twi_rxBuffer, twi_rxBufferIndex);
-      
-      // since we submit rx buffer to "wire" library, we can reset it
-      twi_rxBufferIndex = 0;
+      // this is where a callback would go
       
       break;
     case TW_SR_DATA_NACK:       // data received, returned nack
     case TW_SR_GCALL_DATA_NACK: // data received generally, returned nack
       // nack back at master
-      twi_reply(0);
+      nack();
       break;
     
     // Slave Transmitter
     case TW_ST_SLA_ACK:          // addressed, returned ack
     case TW_ST_ARB_LOST_SLA_ACK: // arbitration lost, returned ack
-      // ready the tx buffer index for iteration
-      twi_txBufferIndex = 0;
-      // set tx buffer length to be zero, to verify if user changes it
-      twi_txBufferLength = 0;
       // request for txBuffer to be filled and length to be set
       // note: user must call twi_transmit(bytes, length) to do this
       // get buffer reader for transmission here
@@ -205,21 +142,18 @@ ISR(TWI_vect)
       // copy data to output register
       TWDR = 0; // <-- put data here
       // if there is more to send, ack, otherwise nack
-      twi_reply(1);
-      // twi_reply(0);
+      ack();
       break;
     case TW_ST_DATA_NACK: // received nack, we are done 
     case TW_ST_LAST_DATA: // received ack, but we are done already!
       // ack future responses
-      twi_reply(1);
+      ack();
       break;
 
     // All
     case TW_NO_INFO:   // no state information
       break;
     case TW_BUS_ERROR: // bus error, illegal stop/start
-      twi_error = TW_BUS_ERROR;
-      //twi_stop();
       break;
   }
 }
