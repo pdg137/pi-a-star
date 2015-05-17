bfae4ae7b30f4c00cfa63827b1639f3ce7297757
Paul Grayson <pdg@alum.mit.edu>
2015-05-17 15:55:30 -0700

    cleaned up and commented code

--- arduino/RPiSlave.cpp
+++ arduino/RPiSlave.cpp
@@ -3,6 +3,8 @@
 #include <util/twi.h>
 
 char data[256] = "Hello, world!";
+unsigned char index;
+unsigned char index_set = 0;
 
 unsigned char RPiSlave::getByte(unsigned char index)
 {
@@ -16,8 +18,12 @@ void RPiSlave::setByte(unsigned char index, unsigned char value)
 
 unsigned char RPiSlave::checkForCommand()
 {
+  // When the CMD_STATUS byte is set to CMD_STATUS_CALL, it means
+  // that a command has been issued by the Raspberry Pi.  The command
+  // number is stored in the CMD_NUMBER byte.  Zero, the default,
+  // means no command.
   if(CMD_STATUS_CALL == data[CMD_STATUS])
-    return data[1];
+    return data[CMD_NUMBER];
   return 0;
 }
 
@@ -48,7 +54,7 @@ void leds(char y, char g, char r)
 
 ISR(TWI_vect)
 {
-  if(RPiSlave::handle_event(TWSR, TWDR))
+  if(RPiSlave::handle_event(TWSR))
     RPiSlave::nack();
   else
     RPiSlave::ack();
@@ -71,43 +77,81 @@ void RPiSlave::nack()
     | (1<<TWINT); // clear interrupt flag
 }
 
-uint8_t RPiSlave::handle_event(unsigned char event, unsigned char write_data)
+void reset_index()
 {
-  static int index;
-  static int index_set = 0;
+  index = 0; // go to the beginning of the array
+  index_set = 0;
+}
+
+// delay to accomodate the Broadcom I2C bug.
+void rpi_delay()
+{
+  delayMicroseconds(10);
+}
+
+// reads the available byte and stores it in index
+void read_index()
+{
+  index = TWDR;
+  index_set = 1;
+}
+
+// Write the lock value to status if we are about to write a command.
+void lock_if_writing_command()
+{
+  if(index < 128)
+    data[CMD_STATUS] = CMD_STATUS_LOCK;
+}
+
+// Write a byte and increment the index.
+void write_and_increment()
+{
+  data[index] = TWDR;
+  index ++;
+}
+
+void read_and_increment()
+{
+  TWDR = data[index];
+  index ++;
+}
+
+// Sets the status to "call" if it is in "lock".
+void call_if_locked_for_command()
+{
+  if(CMD_STATUS_LOCK == data[0])
+    data[CMD_STATUS] = CMD_STATUS_CALL;
+}
+
+uint8_t RPiSlave::handle_event(unsigned char event)
+{
   
   switch(event)
   {
     case TW_SR_SLA_ACK:
-      delayMicroseconds(10);
-      index = 0; // go to the beginning of the array
-      index_set = 0;
+      rpi_delay();
+      reset_index();
       break;
     case TW_SR_DATA_ACK:
-      delayMicroseconds(10);
+      rpi_delay();
       if(!index_set)
       {
-        index = write_data;
-        index_set = 1;
+        read_index();
       }
       else
       {
-        if(index < 128)
-          data[CMD_STATUS] = CMD_STATUS_LOCK;
-        data[index] = write_data;
-        index ++;
+        lock_if_writing_command();
+        write_and_increment();
       }
       break;
     case TW_SR_DATA_NACK:
       return 1;
     case TW_ST_SLA_ACK:
     case TW_ST_DATA_ACK:
-      TWDR = data[index];
-      index ++;
+      read_and_increment();
       break;
     case TW_SR_STOP: // master is done sending
-      if(CMD_STATUS_LOCK == data[0])
-        data[CMD_STATUS] = CMD_STATUS_CALL;
+      call_if_locked_for_command();
       break;
   }
   
--- arduino/RPiSlave.h
+++ arduino/RPiSlave.h
@@ -1,4 +1,5 @@
 static unsigned char CMD_STATUS=0;
+static unsigned char CMD_NUMBER=1;
 static unsigned char CMD_STATUS_LOCK=2;
 static unsigned char CMD_STATUS_CALL=0;
 static unsigned char CMD_STATUS_RETURN=1;
@@ -10,13 +11,13 @@ class RPiSlave
   static void init(unsigned char address);
   static void ack();
   static void nack();
-  static unsigned char handle_event(unsigned char event, unsigned char data);
+  static unsigned char handle_event(unsigned char event);
   static unsigned char getByte(unsigned char index);
   static void setByte(unsigned char index, unsigned char value);
   
   /* Check for a command and return the command # if we are being called. */
   static unsigned char checkForCommand();
   
-  /* Return from a command */
+  /* Indicate that we are done processing a command. */
   static void commandReturn();
 };
